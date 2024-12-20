4a2d3a72ee4292d597a9b96494535599e1016ae8
Revan <84303812+TheRevanite@users.noreply.github.com>
2024-12-20 10:55:35 +0530

    Update main.py
    considers a motor and two cameras, with codes to turn the motor accordingly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import lights, image_processing
+import lights, image_processing, camera_capture, servomotor
 
 #RUNNING PIN SETUP
 pin_mappings=lights.setup()
@@ -16,17 +16,24 @@ ref_images_array=image_processing.image_conversion(og_ref_images)
 while True:
 
     #LOOPING FOR EACH ROAD STARTS HERE 
-    for i in range(4):
+    for i in range(1, 5):
+        #ROTATING THE MOTOR ACCORDINGLY.
+        servomotor.roads2(2)
             
         #DIRECTLY OBTAINING PROCESSED LIVE FEED
-        imagepath=image_processing.preprocess_image()
+        if i == 1 or i == 4:
+            frame1 = camera_capture.capture_camera1()
+            imagepath=image_processing.preprocess_image(frame1)
+        elif i == 2 or i == 3:
+            frame2 = camera_capture.capture_camera2()
+            imagepath=image_processing.preprocess_image(frame2)
 
         #COMPUTING MATCH PERCENTAGE
-        referencepath = ref_images_array[i]
+        referencepath = ref_images_array[i-1]
         match_percentage = image_processing.compute_match_percentage(referencepath, imagepath)
 
         #USING MATCH PERCENTAGE TO DETERMINE DURATION OF SIGNAL AND SENDING IT TO MICROCONTROLLER
-        duration=0 #default, change all the other durations/match criteria later
+        duration=0 #THE DEFAULT VALUE FOR DURATION IS SET AND UPDATED FURTHER IN THE CODE
         if match_percentage<20:
             duration=6
         elif 20<=match_percentage<25:
@@ -36,13 +43,13 @@ while True:
         elif 30<=match_percentage<=100:
             duration=2
         print(f"The match percentage is {match_percentage}, so the green light will be on for {duration} seconds.")
-        #SENDING DURATION FOR TIME.STOP
-        def run():
-            return duration
-
+        if i%2==0: 
+            servomotor.roads2(duration)
+        else:
+            servomotor.roads1(duration)
         #RUNNING LEDS ACCORDINGLY
-        lights.light_on(i+1) #CHECK WHETHER i OR i+1
+        lights.light_on(i) #CHECK WHETHER i OR i+1
         lights.wait(duration)
-        lights.light_off(i+1)
+        lights.light_off(i)
 
         #LOOPING ENDS HERE
