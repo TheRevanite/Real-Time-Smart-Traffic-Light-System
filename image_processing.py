2eed2e40ea6e5f45fbd354dd10ecb6b4d5f0b59f
Revan <84303812+TheRevanite@users.noreply.github.com>
2024-12-20 10:51:47 +0530

    Update image_processing.py
    refined the matching algorithm to count good edges only, hence providing a more accurate match percentage

--- image_processing.py
+++ image_processing.py
@@ -1,27 +1,18 @@
 import cv2
-import numpy as np
-import camera_capture
 
-def preprocess_image():
-    img_path = camera_capture.capture_camera1()
+def preprocess_image(img_path):
     img = cv2.imread(img_path) #Read the original frame
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Convert to grayscale
-    img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0) #Blur the image for better edge detection
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0) #Blur the image for better edge detection
     edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) #Canny Edge Detection
-    edges_colored_1 = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR) #Convert edges to a 3-channel image to display with OpenCV
-    
-    
-    #cv2.imshow('Edge Detection Camera 1', edges_colored_1) #This perhaps shows the original image too
     cv2.imwrite("final_image.jpg", edges)
-    #cv2.waitKey(0)  # Wait for a key press to close the window
-    #cv2.destroyAllWindows()  # Close all OpenCV windows
 
     return "final_image.jpg"
 
 def preprocess_reference(img_path):
     img = cv2.imread(img_path) #Read the original frame
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Convert to grayscale
-    img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0) #Blur the image for better edge detection
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0) #Blur the image for better edge detection
     edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) #Canny Edge Detection
     return edges
 
@@ -51,7 +42,10 @@ def compute_match_percentage(image1, image2):
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True) #Creating BFMatcher object
     matches = bf.match(des1, des2) #Match descriptors
     matches = sorted(matches, key=lambda x: x.distance) #Sort them in the order of their distance
-    num_good_matches = len(matches) #Compute match percentage
-    total_matches = min(len(kp1), len(kp2))
-    match_percentage = (num_good_matches / total_matches) * 100
+    good_matches = [m for m in matches if m.distance < 50]  #Keep matches with distance < 50
+    num_good_matches = len(good_matches) #Count good matches
+    total_matches = len(matches) #Total matches found between the two images
+    keypoints_ratio = min(len(kp1), len(kp2)) / max(len(kp1), len(kp2)) #Calculate the keypoints ratio to balance keypoints of varying images
+    match_percentage = (num_good_matches / total_matches) * keypoints_ratio * 100 #Compute the final match percentage
+
     return match_percentage
